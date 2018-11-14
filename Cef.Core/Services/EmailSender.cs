e8705de754e2bd1d65412679342858b90f8cb453
Chris Golden <crgolden@msn.com>
2018-11-14 04:13:24 -0600

    Adding Email Sender

---
+++ Cef.Core/Services/EmailSender.cs
@@ -0,0 +1,38 @@
+﻿namespace Cef.Core.Services
+{
+    using System.Threading.Tasks;
+    using Options;
+    using Microsoft.AspNetCore.Identity.UI.Services;
+    using Microsoft.Extensions.Options;
+    using SendGrid;
+    using SendGrid.Helpers.Mail;
+
+    public class EmailSender : IEmailSender
+    {
+        private readonly EmailOptions _options;
+
+        public EmailSender(IOptions<EmailOptions> optionsAccessor)
+        {
+            _options = optionsAccessor.Value;
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string message)
+        {
+            var client = new SendGridClient(_options.ApiKey);
+            var msg = new SendGridMessage
+            {
+                From = new EmailAddress(_options.Email, _options.Name),
+                Subject = subject,
+                PlainTextContent = message,
+                HtmlContent = message
+            };
+            msg.AddTo(new EmailAddress(email));
+
+            // Disable click tracking.
+            // See https://sendgrid.com/docs/User_Guide/Settings/tracking.html
+            msg.SetClickTracking(false, false);
+
+            await client.SendEmailAsync(msg);
+        }
+    }
+}
