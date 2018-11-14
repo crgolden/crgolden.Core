d1c20a6e748f4a6ca63abb4f26a3e9e355e69520
Chris Golden <crgolden@msn.com>
2018-11-13 20:32:17 -0600

    Adding Base Controllers and Servcies

---
+++ Cef.Core/Controllers/BaseModelController.cs
@@ -0,0 +1,104 @@
+﻿namespace Cef.Core.Controllers
+{
+    using System;
+    using System.Threading.Tasks;
+    using Interfaces;
+    using Models;
+    using Kendo.Mvc.Extensions;
+    using Kendo.Mvc.UI;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+
+    [Produces("application/json")]
+    [Route("v1/[controller]/[action]")]
+    [ApiController]
+    public abstract class BaseModelController<T> : ControllerBase
+        where T : BaseModel
+    {
+        private readonly IModelService<T> _service;
+        private readonly ILogger<BaseModelController<T>> _logger;
+
+        protected BaseModelController(IModelService<T> service, ILogger<BaseModelController<T>> logger)
+        {
+            _service = service;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public virtual async Task<IActionResult> Index([DataSourceRequest] DataSourceRequest request = null)
+        {
+            var models = _service.Index();
+            return request != null ? Ok(await models.ToDataSourceResultAsync(request)) : Ok(models);
+        }
+
+        [HttpGet("{id:guid}")]
+        public virtual async Task<IActionResult> Details([FromRoute] Guid id)
+        {
+            try
+            {
+                var model = await _service.Details(id);
+                if (model == null)
+                {
+                    return NotFound(id);
+                }
+
+                return Ok(model);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, e.Message);
+                return BadRequest(id);
+            }
+        }
+
+        [HttpPut("{id:guid}")]
+        public virtual async Task<IActionResult> Edit([FromRoute] Guid id, [FromBody] T model)
+        {
+            if (!id.Equals(model?.Id))
+            {
+                return BadRequest(id);
+            }
+
+            try
+            {
+                await _service.Edit(model);
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, e.Message);
+                return BadRequest(model);
+            }
+        }
+
+        [HttpPost]
+        public virtual async Task<IActionResult> Create([FromBody] T model)
+        {
+            try
+            {
+                var created = await _service.Create(model);
+                return Ok(created);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, e.Message);
+                return BadRequest(model);
+            }
+        }
+
+        [HttpDelete("{id:guid}")]
+        public virtual async Task<IActionResult> Delete([FromRoute] Guid id)
+        {
+            try
+            {
+                await _service.Delete(id);
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, e.Message);
+                return BadRequest(id);
+            }
+        }
+    }
+}
---
+++ Cef.Core/Services/BaseModelService.cs
@@ -0,0 +1,56 @@
+﻿namespace Cef.Core.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Interfaces;
+    using Models;
+    using Microsoft.EntityFrameworkCore;
+
+    public abstract class BaseModelService<T> : IModelService<T> where T : BaseModel
+    {
+        protected readonly DbContext Context;
+
+        protected BaseModelService(DbContext context)
+        {
+            Context = context;
+        }
+
+        public virtual IEnumerable<T> Index()
+        {
+            return Context.Set<T>().AsNoTracking();
+        }
+
+        public virtual async Task<T> Details(Guid id)
+        {
+            return await Context.Set<T>().SingleOrDefaultAsync(x => x.Id.Equals(id));
+        }
+
+        public virtual async Task<T> Create(T model)
+        {
+            Context.Add(model);
+            await Context.SaveChangesAsync();
+            return model;
+        }
+
+        public virtual async Task Edit(T model)
+        {
+            var entity = await Context.Set<T>().SingleOrDefaultAsync(x => x.Id.Equals(model.Id));
+            if (entity != null)
+            {
+                entity.Name = model.Name;
+            }
+            await Context.SaveChangesAsync();
+        }
+
+        public virtual async Task Delete(Guid id)
+        {
+            var entity = await Context.Set<T>().SingleOrDefaultAsync(x => x.Id.Equals(id));
+            if (entity != null)
+            {
+                Context.Remove(entity);
+                await Context.SaveChangesAsync();
+            }
+        }
+    }
+}
