142fb4922e33ce19f73f95eb9e7f4cf9b574f5c3
Chris Golden <crgolden@msn.com>
2018-11-16 05:36:00 -0600

    Adding DbContext

---
+++ Cef.Core/Contexts/CefDbContext.cs
@@ -0,0 +1,89 @@
+﻿namespace Cef.Core.Contexts
+{
+    using System;
+    using JetBrains.Annotations;
+    using Models;
+    using Relationships;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    [PublicAPI]
+    public class CefDbContext : IdentityDbContext<User, Role, Guid, UserClaim, UserRole, UserLogin, RoleClaim, UserToken>
+    {
+        public CefDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>(b =>
+            {
+                // Each User can have many UserClaims
+                b.HasMany(e => e.Claims)
+                    .WithOne(e => e.User)
+                    .HasForeignKey(uc => uc.UserId)
+                    .IsRequired();
+
+                // Each User can have many UserLogins
+                b.HasMany(e => e.Logins)
+                    .WithOne(e => e.User)
+                    .HasForeignKey(ul => ul.UserId)
+                    .IsRequired();
+
+                // Each User can have many UserTokens
+                b.HasMany(e => e.Tokens)
+                    .WithOne(e => e.User)
+                    .HasForeignKey(ut => ut.UserId)
+                    .IsRequired();
+
+                // Each User can have many entries in the UserRole join table
+                b.HasMany(e => e.UserRoles)
+                    .WithOne(e => e.User)
+                    .HasForeignKey(ur => ur.UserId)
+                    .IsRequired();
+                b.ToTable("Users");
+            });
+
+            modelBuilder.Entity<UserClaim>(b =>
+            {
+                b.ToTable("UserClaims");
+            });
+
+            modelBuilder.Entity<UserLogin>(b =>
+            {
+                b.ToTable("UserLogins");
+            });
+
+            modelBuilder.Entity<UserToken>(b =>
+            {
+                b.ToTable("UserTokens");
+            });
+
+            modelBuilder.Entity<Role>(b =>
+            {
+                // Each Role can have many entries in the UserRole join table
+                b.HasMany(e => e.UserRoles)
+                    .WithOne(e => e.Role)
+                    .HasForeignKey(ur => ur.RoleId)
+                    .IsRequired();
+
+                // Each Role can have many associated RoleClaims
+                b.HasMany(e => e.RoleClaims)
+                    .WithOne(e => e.Role)
+                    .HasForeignKey(rc => rc.RoleId)
+                    .IsRequired();
+                b.ToTable("Roles");
+            });
+
+            modelBuilder.Entity<RoleClaim>(b =>
+            {
+                b.ToTable("RoleClaims");
+            });
+
+            modelBuilder.Entity<UserRole>(b =>
+            {
+                b.ToTable("UserRoles");
+            });
+        }
+    }
+}
--- Cef.Core/Extensions/WebHostExtensions.cs
+++ Cef.Core/Extensions/WebHostExtensions.cs
@@ -4,24 +4,21 @@
     using Interfaces;
     using JetBrains.Annotations;
     using Microsoft.AspNetCore.Hosting;
-    using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.DependencyInjection;
 
     [PublicAPI]
     public static class WebHostExtensions
     {
-        public static void SetupDatabase<TContext>(this IWebHost webHost) where TContext : DbContext
+        public static async Task<IWebHost> SeedDatabaseAsync(this IWebHost webHost)
         {
             using (var scope = webHost.Services.CreateScope())
             {
                 var services = scope.ServiceProvider;
-                using (var context = services.GetRequiredService<TContext>())
-                {
-                    context.Database.Migrate();
-                    var seedDataService = services.GetRequiredService<ISeedDataService>();
-                    Task.Run(seedDataService.SeedDatabase).Wait();
-                }
+                var seedDataService = services.GetRequiredService<ISeedDataService>();
+                await seedDataService.SeedDatabase();
             }
+
+            return webHost;
         }
     }
 }
