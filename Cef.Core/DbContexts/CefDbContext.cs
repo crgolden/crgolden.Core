345f5d897ea0689b3dc9c9664d1b2b9169b7f641
Chris Golden <crgolden@msn.com>
2018-11-17 08:38:17 -0600

    Adding ModelBuilderExtensions

--- Cef.Core/DbContexts/CefDbContext.cs
+++ Cef.Core/DbContexts/CefDbContext.cs
@@ -1,178 +1,63 @@
 ﻿namespace Cef.Core.DbContexts
 {
     using System;
+    using System.Threading.Tasks;
+    using Extensions;
     using IdentityServer4.EntityFramework.Entities;
+    using IdentityServer4.EntityFramework.Extensions;
+    using IdentityServer4.EntityFramework.Interfaces;
+    using IdentityServer4.EntityFramework.Options;
     using JetBrains.Annotations;
     using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore;
     using Models;
     using Relationships;
-    using UserClaim = Relationships.UserClaim;
 
     [PublicAPI]
-    public class CefDbContext : IdentityDbContext<User, Role, Guid, UserClaim, UserRole, UserLogin, RoleClaim, UserToken>
+    public class CefDbContext : IdentityDbContext<User, Role, Guid, Relationships.UserClaim, UserRole, UserLogin, RoleClaim, UserToken>,
+        IConfigurationDbContext,
+        IPersistedGrantDbContext
     {
+        /// <inheritdoc />
+        /// <param name="options"></param>
         public CefDbContext(DbContextOptions options) : base(options) { }
 
+        /// <inheritdoc />
+        public virtual DbSet<Client> Clients { get; set; }
+
+        /// <inheritdoc />
+        public virtual DbSet<IdentityResource> IdentityResources { get; set; }
+
+        /// <inheritdoc />
+        public virtual DbSet<ApiResource> ApiResources { get; set; }
+
+        /// <inheritdoc />
+        public virtual DbSet<PersistedGrant> PersistedGrants { get; set; }
+
+        /// <inheritdoc />
+        public virtual DbSet<DeviceFlowCodes> DeviceFlowCodes { get; set; }
+
+        /// <inheritdoc cref="IConfigurationDbContext.SaveChangesAsync" />
+        /// <inheritdoc cref="IPersistedGrantDbContext.SaveChangesAsync" />
+        public virtual Task<int> SaveChangesAsync()
+        {
+            return base.SaveChangesAsync();
+        }
+
+        /// <inheritdoc />
+        /// <param name="modelBuilder"></param>
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<User>(b =>
-            {
-                // Each User can have many UserClaims
-                b.HasMany(e => e.Claims)
-                    .WithOne(e => e.User)
-                    .HasForeignKey(uc => uc.UserId)
-                    .IsRequired();
+            modelBuilder.ConfigureIdentityContext();
 
-                // Each User can have many UserLogins
-                b.HasMany(e => e.Logins)
-                    .WithOne(e => e.User)
-                    .HasForeignKey(ul => ul.UserId)
-                    .IsRequired();
+            var configurationStoreOptions = new ConfigurationStoreOptions();
+            modelBuilder.ConfigureClientContext(configurationStoreOptions);
+            modelBuilder.ConfigureResourcesContext(configurationStoreOptions);
 
-                // Each User can have many UserTokens
-                b.HasMany(e => e.Tokens)
-                    .WithOne(e => e.User)
-                    .HasForeignKey(ut => ut.UserId)
-                    .IsRequired();
-
-                // Each User can have many entries in the UserRole join table
-                b.HasMany(e => e.UserRoles)
-                    .WithOne(e => e.User)
-                    .HasForeignKey(ur => ur.UserId)
-                    .IsRequired();
-                b.ToTable("Users");
-            });
-
-            modelBuilder.Entity<UserClaim>(b =>
-            {
-                b.ToTable("UserClaims");
-            });
-
-            modelBuilder.Entity<UserLogin>(b =>
-            {
-                b.ToTable("UserLogins");
-            });
-
-            modelBuilder.Entity<UserToken>(b =>
-            {
-                b.ToTable("UserTokens");
-            });
-
-            modelBuilder.Entity<Role>(b =>
-            {
-                // Each Role can have many entries in the UserRole join table
-                b.HasMany(e => e.UserRoles)
-                    .WithOne(e => e.Role)
-                    .HasForeignKey(ur => ur.RoleId)
-                    .IsRequired();
-
-                // Each Role can have many associated RoleClaims
-                b.HasMany(e => e.RoleClaims)
-                    .WithOne(e => e.Role)
-                    .HasForeignKey(rc => rc.RoleId)
-                    .IsRequired();
-                b.ToTable("Roles");
-            });
-
-            modelBuilder.Entity<RoleClaim>(b =>
-            {
-                b.ToTable("RoleClaims");
-            });
-
-            modelBuilder.Entity<UserRole>(b =>
-            {
-                b.ToTable("UserRoles");
-            });
-
-            // ConfigurationDbContext
-            modelBuilder.Entity<Client>(b =>
-            {
-                b.ToTable("Clients");
-            });
-
-            modelBuilder.Entity<ClientSecret>(b =>
-            {
-                b.ToTable("ClientSecrets");
-            });
-
-            modelBuilder.Entity<ClientGrantType>(b =>
-            {
-                b.ToTable("ClientGrantTypes");
-            });
-
-            modelBuilder.Entity<ClientRedirectUri>(b =>
-            {
-                b.ToTable("ClientRedirectUris");
-            });
-
-            modelBuilder.Entity<ClientPostLogoutRedirectUri>(b =>
-            {
-                b.ToTable("ClientPostLogoutRedirectUris");
-            });
-            
-            modelBuilder.Entity<ClientScope>(b =>
-            {
-                b.ToTable("ClientScopes");
-            });
-            
-            modelBuilder.Entity<ClientIdPRestriction>(b =>
-            {
-                b.ToTable("ClientIdPRestrictions");
-            });
-            
-            modelBuilder.Entity<ClientClaim>(b =>
-            {
-                b.ToTable("ClientClaims");
-            });
-            
-            modelBuilder.Entity<ClientCorsOrigin>(b =>
-            {
-                b.ToTable("ClientCorsOrigins");
-            });
-            
-            modelBuilder.Entity<ClientProperty>(b =>
-            {
-                b.ToTable("ClientProperties");
-            });
-            
-            modelBuilder.Entity<IdentityResource>(b =>
-            {
-                b.ToTable("IdentityResources");
-            });
-
-            modelBuilder.Entity<IdentityClaim>(b =>
-            {
-                b.ToTable("IdentityClaims");
-            });
-
-            modelBuilder.Entity<ApiResource>(b =>
-            {
-                b.ToTable("ApiResources");
-            });
-
-            modelBuilder.Entity<ApiSecret>(b =>
-            {
-                b.ToTable("ApiSecrets");
-            });
-
-            modelBuilder.Entity<ApiScope>(b =>
-            {
-                b.ToTable("ApiScopes");
-            });
-
-            modelBuilder.Entity<ApiResourceClaim>(b =>
-            {
-                b.ToTable("ApiResourceClaims");
-            });
-
-            // PersistedGrantDbContext
-            modelBuilder.Entity<PersistedGrant>(b =>
-            {
-                b.ToTable("PersistedGrants");
-            });
+            var operationalStoreOptions = new OperationalStoreOptions();
+            modelBuilder.ConfigurePersistedGrantContext(operationalStoreOptions);
         }
     }
 }
