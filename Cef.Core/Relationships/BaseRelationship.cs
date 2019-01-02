366726b2a209af4fa803f5919d1df47e35e50c43
Chris Golden <crgolden@msn.com>
2019-01-01 19:38:17 -0600

    Adding JsonProperty

--- Cef.Core/Relationships/BaseRelationship.cs
+++ Cef.Core/Relationships/BaseRelationship.cs
@@ -10,25 +10,31 @@
     public abstract class BaseRelationship<T1, T2> where T1 : BaseModel where T2 : BaseModel
     {
         [Required]
+        [JsonProperty("model1Id")]
         public Guid Model1Id { get; set; }
 
         [Required]
+        [JsonProperty("model1Name")]
         public string Model1Name { get; set; }
 
         [JsonIgnore]
         public T1 Model1 { get; set; }
 
         [Required]
+        [JsonProperty("model2Id")]
         public Guid Model2Id { get; set; }
 
         [Required]
+        [JsonProperty("model2Name")]
         public string Model2Name { get; set; }
 
         [JsonIgnore]
         public T2 Model2 { get; set; }
 
+        [JsonProperty("created")]
         public DateTime Created { get; set; }
 
+        [JsonProperty("updated")]
         public DateTime? Updated { get; set; }
     }
 }
