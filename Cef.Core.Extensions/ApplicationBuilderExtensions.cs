dd0a908c0e8203d7d50ff64110c65c5097312415
Chris Golden <crgolden@msn.com>
2019-02-12 00:23:31 -0600

    Moving AddSwagger

---
+++ Cef.Core.Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace Cef.Core.Extensions
+{
+    using System.Diagnostics.CodeAnalysis;
+    using JetBrains.Annotations;
+    using Microsoft.AspNetCore.Builder;
+
+    [PublicAPI]
+    [ExcludeFromCodeCoverage]
+    public static class ApplicationBuilderExtensions
+    {
+        public static void UseSwagger(
+            this IApplicationBuilder app,
+            string name,
+            string url = "/swagger/v1/swagger.json",
+            string routePrefix = "")
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(setup =>
+            {
+                setup.SwaggerEndpoint(url, name);
+                setup.RoutePrefix = routePrefix;
+                setup.DocumentTitle = name;
+            });
+        }
+    }
+}
