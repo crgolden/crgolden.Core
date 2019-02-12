29d0738cefee195e8e4573a6a989952c43c58cbf
Chris Golden <crgolden@msn.com>
2019-02-12 07:23:51 -0600

    Moving thumbnails and images from Azure

--- Cef.Core.Options/StorageOptions.cs
+++ Cef.Core.Options/StorageOptions.cs
@@ -7,6 +7,10 @@
     [ExcludeFromCodeCoverage]
     public class StorageOptions
     {
+        public string ImageContainer { get; set; }
+
+        public string ThumbnailContainer { get; set; }
+
         public AzureBlobStorage AzureBlobStorage { get; set; }
     }
 
@@ -15,10 +19,6 @@
     public class AzureBlobStorage
     {
         public string AccountName { get; set; }
-
-        public string ImageContainer { get; set; }
-
-        public string ThumbnailContainer { get; set; }
 
         public string AccountKey { get; set; }
     }
