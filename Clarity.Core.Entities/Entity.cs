8f0ba69c3b77a99fa71cae1b0d2bd1f0db027cc3
Chris Golden <crgolden@msn.com>
2019-02-16 21:26:11 -0600

    publishing missing packages

--- Clarity.Core.Entities/Entity.cs
+++ Clarity.Core.Entities/Entity.cs
@@ -1,11 +1,14 @@
 ﻿namespace Clarity.Core
 {
     using System;
+    using Newtonsoft.Json;
 
     public abstract class Entity
     {
+        [JsonProperty("created")]
         public DateTime Created { get; }
 
+        [JsonProperty("updated")]
         public DateTime? Updated { get; set; }
 
         protected Entity()
