d49dc753e5371d18280ae2a54c27413ebb259912
Chris Golden <crgolden@msn.com>
2019-02-24 03:36:47 -0600

    Adding profiles

---
+++ Clarity.Core.Profiles/Profile.cs
@@ -0,0 +1,13 @@
+﻿namespace Clarity.Core.Profiles
+{
+    public abstract class Profile : AutoMapper.Profile
+    {
+        protected Profile()
+        {
+            CreateMap<Entity, Model>()
+                .IncludeAllDerived()
+                .ForMember(dest => dest.Created, opt => opt.MapFrom(src => src.Created))
+                .ForMember(dest => dest.Updated, opt => opt.MapFrom(src => src.Updated));
+        }
+    }
+}
