5f6c295f5bb609f4196598283d68c24103b33753
Chris Golden <crgolden@msn.com>
2019-02-26 17:37:44 -0600

    Cancellation token on migrate

--- Clarity.Core.Extensions/WebHostExtensions.cs
+++ Clarity.Core.Extensions/WebHostExtensions.cs
@@ -1,29 +1,19 @@
 ﻿namespace Clarity.Core
 {
+    using System.Threading;
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Hosting;
-    using Microsoft.Azure.ServiceBus;
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.DependencyInjection;
 
     public static class WebHostExtensions
     {
-        public static async Task<IWebHost> MigrateDatabaseAsync(this IWebHost webHost)
+        public static async Task<IWebHost> MigrateDatabaseAsync(this IWebHost webHost, CancellationToken token)
         {
             using (var scope = webHost.Services.CreateScope())
             {
                 var context = scope.ServiceProvider.GetRequiredService<DbContext>();
-                await context.Database.MigrateAsync();
-            }
-
-            return webHost;
-        }
-
-        public static IWebHost GetQueueClients(this IWebHost webHost)
-        {
-            using (var scope = webHost.Services.CreateScope())
-            {
-                scope.ServiceProvider.GetServices<IQueueClient>();
+                await context.Database.MigrateAsync(token);
             }
 
             return webHost;
